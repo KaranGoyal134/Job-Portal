a72b1f786ba79253c666e3e1e9c1275f95e7d516
Karan Goyal <karangoyal134@gmail.com>
2026-04-27 17:07:15 +0000

    Made all the final changes

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "state-file-locking-practise"
-    key            = "job-portal/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
